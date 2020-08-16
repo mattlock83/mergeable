350d858ec7b15d50cbd6868f84bcfb11850249f5
mattlock83 <lock.matt@gmail.com>
2020-08-16 21:32:56 +1000

    Fix up whitespace in changes

--- action.js
+++ action.js
@@ -1,6 +1,6 @@
 // Set config to config_file input value from Actions if provided
 if (process.env.INPUT_CONFIG_FILE) {
-  process.env.CONFIG_PATH = process.env.INPUT_CONFIG_FILE	
+  process.env.CONFIG_PATH = process.env.INPUT_CONFIG_FILE
 }
 
 // Start Mergeable using the Probot Actions Adapter
