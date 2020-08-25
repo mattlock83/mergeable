a923d99727f44374eeedc820f2a52a5a9fa14c23
mattlock83 <lock.matt@gmail.com>
2020-08-25 23:21:35 +1000

    Switch to review comments and ncc
    - Docker is taking roughly 45-60 to run, move to NCC packaged distribution which takes a few seconds
    - Add a results as review action to communicate result of mergeable on a PR instead of via a check status

--- action.js
+++ action.js
@@ -1,4 +1,7 @@
-// Set config to config_file input value from Actions if provided
+// Running as a github action
+process.env.APP_NAME = 'github-actions'
+
+// Set mergeable yml config to config_file input from Actions workflow if provided
 if (process.env.INPUT_CONFIG_FILE) {
   process.env.CONFIG_PATH = process.env.INPUT_CONFIG_FILE
 }
